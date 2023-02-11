44cb4032ba39488f1bec850bb97adfdc74686c39
Ranichel Louisville C. Rivera <billy100007@gmail.com>
2023-02-11 13:18:24 +0800

    My Android #1st Activity Bwhahahaha

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.helloandroid;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivty";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is verbose log");
+        Log.d(TAG, "This is debug log");
+        Log.i(TAG, "This is an info log");
+        Log.w(TAG, "This is a warn log");
+        Log.e(TAG, "This is an error log");
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onCLick(View v){
+                Log.i(TAG,"Button CLicked");
+            }
+        });
+
     }
 }
