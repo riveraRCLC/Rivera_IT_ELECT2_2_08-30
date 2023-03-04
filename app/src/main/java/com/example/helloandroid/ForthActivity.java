0648c1356e412f0de0f44cac551652aacf2d82fc
riveraRCLC <billy100007@gmail.com>
2023-03-04 12:56:47 +0800

    4th 5th activity

--- app/src/main/java/com/example/helloandroid/ForthActivity.java
+++ app/src/main/java/com/example/helloandroid/ForthActivity.java
@@ -8,14 +8,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class ThirdActivity extends AppCompatActivity {
+public class ForthActivity extends AppCompatActivity {
 
 
-    private static final String TAG = "ThirdActivity";
+
+    private static final String TAG = "ForthActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_forth);
+
 
         Log.v(TAG, "This is a verbose log.");
         Log.d(TAG,"This is a debug log.");
@@ -23,13 +26,13 @@ public class ThirdActivity extends AppCompatActivity {
         Log.w(TAG, "This is a warn log.");
         Log.e(TAG, "This is an error log.");
 
-        Button button = (Button) findViewById(R.id.doneButton);
+        Button button = (Button) findViewById(R.id.FifActBut);
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Log.i(TAG, "Button Clicked");
-                Intent intent = new Intent( ThirdActivity.this, ForthActivity.class);
+                Intent intent = new Intent( ForthActivity.this, FifthActivity.class);
                 startActivity(intent);
             }
         });
