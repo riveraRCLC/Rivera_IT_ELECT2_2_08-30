136a79f17dd368dc4871559f57b5ef679b83e4bc
riveraRCLC <billy100007@gmail.com>
2023-03-11 13:04:49 +0800

    FRAG 2ND ATTEMPT

--- app/src/main/java/com/example/helloandroid/FragmentB.java
+++ app/src/main/java/com/example/helloandroid/FragmentB.java
@@ -2,7 +2,7 @@ package com.example.helloandroid;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import android.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
